90dd359659357932d152869b50bfe13754568c67
Daniel Schwarz <tomatobrew@gmail.com>
2022-05-27 23:36:08 +0200

    updated gen

--- graphs/gen/present.sh
+++ graphs/gen/present.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3 ../scripts/parse_jinja.py -d ../scripts/vars.json -t ../le_present.dot.j2 | dot -Tsvg > present.svg
+# shitty script building the svgs, to be replaced with cmake or something else offering build targets and file types for output
+if [[ ! -d $1 ]]; then
+    echo "not a valid path"
+    exit 1
+fi
+
+for template in $1/*.dot.j2; do
+    out="$(cut -d'.' -f1 <<< $(basename "$template")).svg"
+    python3 ../scripts/parse_jinja.py -d ../scripts/vars.json -t $template | dot -Tsvg > $out
+done
